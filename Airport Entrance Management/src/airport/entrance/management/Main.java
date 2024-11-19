f571447dccc49159082afd5cc8999b3972b8d4b5
rakibhassan66 <rakibhassan.rh66@gmail.com>
2024-11-19 17:00:41 +0600

    Mass changes

--- Airport Entrance Management/src/airport/entrance/management/Main.java
+++
@@ -1,9 +0,0 @@
-package airport.entrance.management;
-
-public class Main {
-    
-    public static void main(String[] args) {
-        Entrance entrance = new Entrance();
-        entrance.start();
-    }
-}
